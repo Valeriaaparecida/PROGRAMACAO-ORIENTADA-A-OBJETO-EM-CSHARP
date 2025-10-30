9f34525b7ec3af0bb95c6217e6ea883f9ca033c4
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-10-29 22:23:02 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Exercicio1;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Pessoa p1 = new Pessoa();
+           Pessoa p2 = new Pessoa();
+           Console.WriteLine("Dados da primeira pessoa:"); 
+           Console.Write("Nome: ");
+           p1.Nome = Console.ReadLine();
+           Console.Write("Idade: ");
+           p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else if (p2.Idade > p1.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Ambas as pessoas têm a mesma idade.");
+            }
+        }
+    }
+}
---
+++ Trinagulo/Trinagulo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Trinagulo;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            Console.WriteLine($"Área do triângulo X: {areaX:F3}");
+            Console.WriteLine($"Área do triângulo Y: {areaY:F3}");
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Área do triângulo X é maior");
+            }
+            else
+            {
+                Console.WriteLine("Área do triângulo Y é maior");
+            }
+        }
+    }
+}
