22281d25d6f2ecdc254b98c4b8a8f68dc111e9bd
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-10-30 17:29:59 -0300

    adicionando exercicio

---
+++ Funcionario/Funcionario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario func1 = new Funcionario();
+            Funcionario func2 = new Funcionario();
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            func1.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func1.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            func2.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func2.SalarioBruto = double.Parse(Console.ReadLine());
+            double salarioMedio = (func1.SalarioBruto + func2.SalarioBruto) / 2.0;
+            Console.WriteLine($"Salário médio: {salarioMedio:F2}");
+        }
+    }
+}
